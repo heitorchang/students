abccaafb187a02ce3598177395fb6dedc64d3e8d
Heitor Chang <heitorchang@gmail.com>
2019-10-08 11:43:53 -0300

    admin records

---
+++ records/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+from django.utils import timezone
+
+
+def format_lesson_time(t):
+    return timezone.localtime(t).strftime("%d/%m/%Y %H:%M")
+
+
+class Student(models.Model):
+    teacher = models.ForeignKey(User, related_name="records_students", on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    phone = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(blank=True)
+    lesson_weekday = models.IntegerField(blank=True, null=True)  # 0 = Monday
+    lesson_time = models.TimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    teacher = models.ForeignKey(User, related_name="records_lessons", on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, related_name="records_lessons", on_delete=models.CASCADE)
+    start_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return "{} {} to {}".format(self.student, format_lesson_time(self.start_at), format_lesson_time(self.end_at))
