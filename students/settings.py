4d7e8987fbd55fe876ae6441cf050e97e97d9c82
Me 269818 <269818@hellokey.nfshost.com>
2019-10-11 18:11:08 +0000

    static root

--- students/settings.py
+++ students/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/public/static/'
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
