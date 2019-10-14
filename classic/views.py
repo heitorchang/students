7abd219b51eaa8b65107132cefa27bffbebdf26a
Heitor Chang <heitorchang@gmail.com>
2019-10-14 10:28:46 -0300

    remove default time fields

--- classic/views.py
+++ classic/views.py
@@ -12,22 +12,10 @@ def studentlist(request):
         student_phone = request.POST['phone']
         student_email = request.POST['email']
 
-        if request.POST['weekday'].strip() == "":
-            student_weekday = None
-        else:
-            student_weekday = int(request.POST['weekday'])
-
-        if request.POST['hour'] == "" or request.POST['minutes'] == "":
-            student_time = None
-        else:
-            student_time = request.POST['hour'] + request.POST['minutes']
-
         Student.objects.create(teacher=student_teacher,
                                name=student_name,
                                phone=student_phone,
-                               email=student_email,
-                               lesson_weekday=student_weekday,
-                               lesson_time=student_time)
+                               email=student_email)
         
         return redirect("classic:studentlist")
     
@@ -37,10 +25,6 @@ def studentlist(request):
           name: '',
           phone: '',
           email: '',
-          weekday: '',
-          time: '',
-          hours: '',
-          minutes: '',
         }
         """
 
@@ -62,49 +46,20 @@ def studentdetail(request, student_id):
         new_phone = request.POST['phone']
         new_email = request.POST['email']
 
-        if request.POST['weekday'].strip() == "":
-            new_weekday = None
-        else:
-            new_weekday = int(request.POST['weekday'])
-
-        if request.POST['hour'] == "" or request.POST['minutes'] == "":
-            new_time = None
-        else:
-            new_time = request.POST['hour'] + request.POST['minutes']
-
         student.teacher = new_teacher
         student.name = new_name
         student.phone = new_phone
         student.email = new_email
-        student.lesson_weekday = new_weekday
-        student.lesson_time = new_time
         student.save()
         
         return redirect("classic:studentlist")
         
     else:        
-        if student.lesson_weekday is not None:
-            student_weekday = student.lesson_weekday
-        else:
-            student_weekday = ''
-
-        if student.lesson_time is not None:
-            time_vals = str(student.lesson_time).split(":")
-            student_hours = time_vals[0]
-            student_minutes = ":" + time_vals[1]
-        else:
-            student_hours = ""
-            student_minutes = ""
-        
         vueStudent = f"""
         student: {{
           name: '{student.name}',
           phone: '{student.phone}',
           email: '{student.email}',
-          weekday: '{student_weekday}',
-          time: '{student.lesson_time}',
-          hours: '{student_hours}',
-          minutes: '{student_minutes}',
         }}
         """
     
